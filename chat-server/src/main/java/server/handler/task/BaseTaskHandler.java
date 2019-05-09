e05c0d8d8d4159ca5c75a6bc7c2fc41e6a918014
dfpeng <dfpeng@ctrip.com>
2019-05-09 10:40:56 +0800

    线程池

--- chat-server/src/main/java/server/handler/task/BaseTaskHandler.java
+++ chat-server/src/main/java/server/handler/task/BaseTaskHandler.java
@@ -1,8 +1,43 @@
 package server.handler.task;
 
+import domain.Response;
+import domain.Task;
+import server.exception.TaskException;
+import server.http.HttpConnectionManager;
+import server.task.TaskManagerThread;
+import util.ProtoStuffUtil;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
 /**
  * @Author: Dunfu Peng
  * @Date: 2019/4/5 14:12
  */
-public class BaseTaskHandler {
+public abstract class BaseTaskHandler implements Runnable{
+    protected Task info;
+    protected HttpConnectionManager manager;
+
+    abstract protected Response process() throws IOException,InterruptedException;
+
+    abstract protected void init(TaskManagerThread parentThread);
+
+    @Override
+    public void run() {
+        try {
+            info.getReceiver().write(ByteBuffer.wrap(ProtoStuffUtil.serialize(process())));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            throw new TaskException(info);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new TaskException(info);
+        }
+    }
+
+    public void init(Task info,HttpConnectionManager manager,TaskManagerThread parentThread){
+        this.info=info;
+        this.manager=manager;
+        init(parentThread);
+    }
 }
--- chat-server/src/main/java/server/task/TaskManagerThread.java
+++ chat-server/src/main/java/server/task/TaskManagerThread.java
@@ -1,16 +1,23 @@
 package server.task;
 
 import domain.Task;
+import lombok.extern.slf4j.Slf4j;
 import server.exception.factory.ExceptionHandlingThreadFactory;
+import server.handler.task.BaseTaskHandler;
 import server.http.HttpConnectionManager;
 import server.util.SpringContextUtil;
 
+import java.io.IOException;
 import java.util.concurrent.*;
 
 /**
+ * 消费者
+ * 负责从阻塞队列中取出任务并提交给线程池
+ *
  * @Author: Dunfu Peng
  * @Date: 2019/4/5 13:00
  */
+@Slf4j
 public class TaskManagerThread extends Thread{
 
     private ExecutorService taskPool;
@@ -23,7 +30,44 @@ public class TaskManagerThread extends Thread{
         this.taskPool=new ThreadPoolExecutor(
                 5,10,1000,
                 TimeUnit.MICROSECONDS,new ArrayBlockingQueue<>(10),
-                new ExceptionHandlingThreadFactory(SpringContextUtil)
-        )
+                new ExceptionHandlingThreadFactory(SpringContextUtil.getBean("taskExceptionHandler")),
+                new ThreadPoolExecutor.CallerRunsPolicy()
+        );
+        this.taskBlockingQueue=taskBlockingQueue;
+        this.httpConnectionManager=SpringContextUtil.getBean("httpConnectionManager");
+        this.crawlerPool=new ThreadPoolExecutor(
+                5,10,1000,
+                TimeUnit.MILLISECONDS,new ArrayBlockingQueue<>(10),
+                new ThreadPoolExecutor.CallerRunsPolicy()
+        );
+    }
+
+    public void shutdown(){
+        taskPool.shutdown();
+        crawlerPool.shutdown();
+        Thread.currentThread().interrupt();
+    }
+
+    @Override
+    public void run() {
+        Task task;
+        try {
+            while (!Thread.currentThread().isInterrupted()){
+                task=taskBlockingQueue.take();
+                log.info("{}已从阻塞队列中取出",task.getReceiver().getRemoteAddress());
+                BaseTaskHandler taskHandler=SpringContextUtil.getBean("BaseTaskHandler",task.getType().toString().toLowerCase());
+                taskHandler.init(task,httpConnectionManager,this);
+                System.out.println(taskHandler);
+                taskPool.execute(taskHandler);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ExecutorService getCrawlerPool(){
+        return crawlerPool;
     }
 }
